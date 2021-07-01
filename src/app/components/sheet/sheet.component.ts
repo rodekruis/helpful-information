7e1bc3571e7379f727c6476d5275685f2612a515
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-07-01 15:42:35 +0200

    feat: restyle sheets component

--- src/app/components/sheet/sheet.component.ts
+++ src/app/components/sheet/sheet.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { toKebabCase } from 'src/app/shared/utils';
 
 @Component({
   selector: 'sheet',
@@ -9,5 +11,9 @@ export class SheetComponent {
   @Input()
   sheetName: string;
 
-  constructor() {}
+  constructor(private router: Router) {}
+
+  public onSheetClick = () => {
+    this.router.navigate([toKebabCase(this.sheetName)]);
+  };
 }
