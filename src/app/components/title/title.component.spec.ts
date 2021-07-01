330abbb83b1edea53927f440582ae17f1c9ed86f
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2021-07-01 18:00:59 +0200

    fix: format error

--- src/app/components/title/title.component.spec.ts
+++ src/app/components/title/title.component.spec.ts
@@ -1,6 +1,5 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TitleComponent } from './title.component';
 
 describe('TitleComponent', () => {
@@ -9,10 +8,9 @@ describe('TitleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TitleComponent ],
+      declarations: [TitleComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
