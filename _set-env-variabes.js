a510242c87308a7eb11bd0a4006c7193bc382eee
Elwin Schmitz <eschmitz@redcross.nl>
2022-01-27 23:21:51 +0100

    Simplify local ENV-variable handling

--- _set-env-variabes.js
+++ _set-env-variabes.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const dotenv = require('dotenv');
+
+// Load environment-variables from .env file
+dotenv.config({
+  debug: process.env.DEBUG,
+});
 
 const configFileTemplate = require('./src/environments/environment.prod.ts.template.js');
 const targetPath = './src/environments/environment.prod.ts';
