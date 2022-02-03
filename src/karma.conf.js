742290fd1a5471e9572685957e9aec2c3ff9c610
Elwin Schmitz <eschmitz@redcross.nl>
2022-02-03 11:47:54 +0100

    Upgrade test-tools + configuration

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/6.3/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -13,19 +13,23 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      captureConsole: true,
+      clearContext: false,
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['html', 'text-summary'],
       fixWebpackSourcePaths: true,
+      combineBrowserReports: true,
+      skipFilesWithNoCoverage: true,
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['dots', 'progress', 'kjhtml', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
+    autoWatch: true,
+    restartOnFileChange: true,
   });
 };
