5be711c8d3bf7d98e98d0afcadb22f467f9feb88
Elwin Schmitz <eschmitz@redcross.nl>
2024-02-05 10:06:59 +0100

    test: Include local-data as test-data
    + Include test for 'syntax trick' to render 'empty spaces' in the Region-list

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
 ## [Unreleased](https://github.com/rodekruis/helpful-information/compare/v23.11.0...main)
 
+### Removed
+
+- Back-button and related logging/logic
+
+### Added
+
+- Local data - A script to download (private) Google Sheets at build-time
+
 ---
 
 ## 2023-11-30: [v23.11](https://github.com/rodekruis/helpful-information/releases/tag/v23.11.0)
--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -22,14 +22,15 @@ export const environment = {
   errorRetry: 'Retest',
 
   // Regions:
-  regions: 'test',
-  regionsLabels: 'Test',
-  regionsSheetIds: 'test-id',
+  regions: 'test, ,test-nl,test-ar',
+  regionsLabels: 'Test, ,Test NL,Test AR',
+  regionsSheetIds:
+    'test-sheet-id-1, ,test-sheet-id-2-language-nl,test-sheet-id-3-language-ar',
 
   // Third-party tokens:
   // Google Sheets API:
   google_sheets_api_key: 'test-api-key',
-  google_sheets_api_url: 'https://example.org/api',
+  google_sheets_api_url: '/data',
 
   // Application Insights
   appInsightsConnectionString: '',
